a9bad5bddb62030dab5abda941bb75ed5833cb58
Aleksey <rusal@bk.ru>
2022-02-11 03:13:19 +0300

    fix

--- tmodel.py
+++ tmodel.py
@@ -179,41 +179,43 @@ def tmodel(text, topic_num):
     # import webbrowser
     # webbrowser.open_new("vis.html")
 
-    # # Рисуем слова
-    # # 1. Wordcloud of Top N words in each topic
-    # def drawWords():
-    #     from matplotlib import pyplot as plt
-    #     from wordcloud import WordCloud, STOPWORDS
-    #     import matplotlib.colors as mcolors
-    #     import nltk;
-    #     nltk.download('stopwords')
-    #     from nltk.corpus import stopwords
-    #     stop_words = stopwords.words('russian')
-    #     stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
-    #     cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
-    #     cloud = WordCloud(stopwords=stop_words,
-    #                       background_color='white',
-    #                       width=2500,
-    #                       height=1800,
-    #                       max_words=10,
-    #                       colormap='tab10',
-    #                       color_func=lambda *args, **kwargs: cols[i],
-    #                       prefer_horizontal=1.0)
-    #     topics = lda_model.show_topics(formatted=False)
-    #     fig, axes = plt.subplots(2, 2, figsize=(10, 10), sharex=True, sharey=True)
-    #     for i, ax in enumerate(axes.flatten()):
-    #         fig.add_subplot(ax)
-    #         topic_words = dict(topics[i][1])
-    #         cloud.generate_from_frequencies(topic_words, max_font_size=300)
-    #         plt.gca().imshow(cloud)
-    #         plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
-    #         plt.gca().axis('off')
-    #     plt.subplots_adjust(wspace=0, hspace=0)
-    #     plt.axis('off')
-    #     plt.margins(x=0, y=0)
-    #     plt.tight_layout()
-    #     plt.show()
-    # # drawWords()
+    # Рисуем слова
+    # 1. Wordcloud of Top N words in each topic
+    def drawWords():
+        from matplotlib import pyplot as plt
+        from wordcloud import WordCloud, STOPWORDS
+        import matplotlib.colors as mcolors
+        import nltk;
+        nltk.download('stopwords')
+        from nltk.corpus import stopwords
+        stop_words = stopwords.words('russian')
+        stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+        cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+        cloud = WordCloud(stopwords=stop_words,
+                          background_color='white',
+                          width=2500,
+                          height=1800,
+                          max_words=10,
+                          colormap='tab10',
+                          color_func=lambda *args, **kwargs: cols[i],
+                          prefer_horizontal=1.0)
+        topics = lda_model.show_topics(formatted=False)
+        fig, axes = plt.subplots(2, 2, figsize=(10, 10), sharex=True, sharey=True)
+        for i, ax in enumerate(axes.flatten()):
+            fig.add_subplot(ax)
+            topic_words = dict(topics[i][1])
+            cloud.generate_from_frequencies(topic_words, max_font_size=300)
+            plt.gca().imshow(cloud)
+            plt.gca().set_title('Тема ' + str(i+1), fontdict=dict(size=16))
+            plt.gca().axis('off')
+        plt.subplots_adjust(wspace=0, hspace=0)
+        plt.axis('off')
+        plt.margins(x=0, y=0)
+        plt.tight_layout()
+        # plt.show()
+        plt.savefig('./static/wc.png')
+        
+    drawWords()
 
 
 
